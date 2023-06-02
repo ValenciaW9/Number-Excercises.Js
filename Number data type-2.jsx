5eb441c6f791a019d887a2a92b29163832b08066
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-06-02 18:47:53 -0400

    Numbers Javascript
    Numbers Javascript Excercises

---
+++ Number data type-2.jsx
@@ -0,0 +1,12 @@
+//console.log("hello world!")
+//console.log("hey programmers!")
+
+
+console.log(1+1);
+console.log(2.5 -2); 
+console.log(3*4);
+console.log(99/9);
+//multiplication/division, addition/substraction
+
+//Modulo (%)
+console.log(22 % 5);
---
+++ numberexcercises.js
@@ -0,0 +1,41 @@
+1. What will the code above print out?
+
+console.log( 9 + 4);
+13
+2. What will the code above print out?
+
+console.log(2 + 4 * 5);
+22
+
+console.log((2 + 4) * 5);
+30
+
+3. what will the code above print out??
+console.log(9/2);
+4.5
+4. What will the code above print out?
+
+console.log(25 % 7);
+4
+console.log(34 % 5);
+3
+5. Whatt will the code above print out?
+console.log(7 % 25);
+7
+6. What will the code above print out?
+console.log(3 % 12):
+3
+7.What will the code above print out?
+console.log(34 % 5);
+4
+8. What will the code above print out?
+console.log(30 % 5);
+6= 30/5.5
+
+9. What will the code above print out?
+console.log(56 % 2);
+0
+10. What will the code above print out?
+console.log(99 % 2);
+1
+console.log(56 % 2);
